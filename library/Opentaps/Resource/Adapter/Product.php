0cf33d0641c1cc61716954d49e370b4449b2b539
Alexander Korzh <alexander.korzh@gmail.com>
2011-10-12 13:45:18 -0400

    Applied a QUICK AND DIRTY fix for categories and prices. Categories are mapped by product SKU. It also removes inactive and virtual products from the feed.

--- library/Opentaps/Resource/Adapter/Product.php
+++ library/Opentaps/Resource/Adapter/Product.php
@@ -34,13 +34,67 @@ class Opentaps_Resource_Adapter_Product extends Opentaps_Resource_Adapter_Abstra
         $this->setResourcePath(sprintf(self::RESOURCE_PATH_PRODUCTS, $storeId, $catalogId));
         $r = $this->request('GET', 'product');
 
-        // TODO remove, temporary solution for prices, has to be implemented in OT
+        // TODO remove, temporary solution for prices and else, has to be implemented in OT
         for ($i = 0; $i < count($r->response->dataset); $i++) {
+            if (
+                (!isset($r->response->dataset[$i]->isActive) || $r->response->dataset[$i]->isActive == "N") ||
+                $r->response->dataset[$i]->isVirtual == "Y"
+            ) {
+                array_splice($r->response->dataset, $i, 1);
+                $i--;
+                continue;
+            }
+
             $r->response->dataset[$i]->prices = array(
-                'listPrice' => rand(1, 1000),
-                'basePrice' => rand(1, 1000),
-                'defaultPrice' => rand(1, 1000),
+                'listPrice' => 0,
+                'basePrice' => 0,
+                'defaultPrice' => $r->response->dataset[$i]->price,
             );
+
+            if (isset($r->response->dataset[$i]->longDescription)) {
+                $r->response->dataset[$i]->longDescription = str_ireplace("&#xd;", "", $r->response->dataset[$i]->longDescription);
+            }
+
+/*
+Phones and Terminals:       1   SPS-PNT-#####
+Emergency Equipment:        2   SPS-EE-#####
+Voice and Data Services:    3   SPS-SVC-#####
+Accessories                 4
+Antennas:                   5   SPS-ANT-#####
+Cables and Adapters:        6   SPS-CAN-#####
+Cases and Holsters:         7   SPS-CSE-#####
+Chargers and Batteries:     8   SPS-CNB-#####
+Docking Stations:           9   SPS-DOC-#####
+Hands-Free:                 10  SPS-HDF-#####
+Handsets:                   11  SPS-HND-#####
+Manuals:                    12  SPS-MAN-#####
+Mounting Solutions:         13  SPS-MNT-#####
+Various:                    14  SPS-VAR-#####
+*/
+
+            $categories = array(
+                'SPS-PNT' => 1,
+                'SPS-EE'  => 2,
+                'SPS-SVC' => 3,
+                'SPS-ANT' => 5,
+                'SPS-CAN' => 6,
+                'SPS-CSE' => 7,
+                'SPS-CNB' => 8,
+                'SPS-DOC' => 9,
+                'SPS-HDF' => 10,
+                'SPS-HND' => 11,
+                'SPS-MAN' => 12,
+                'SPS-MNT' => 13,
+                'SPS-VAR' => 14,
+            );
+
+            preg_match("/^[^-]+-[^-]+/", $r->response->dataset[$i]->productId, $matches);
+//            var_dump('$matches', $matches);
+            $categoryIdBySKU = $matches[0];
+
+            if (array_key_exists($categoryIdBySKU, $categories)) {
+                $r->response->dataset[$i]->primaryProductCategoryId = (String) $categories[$categoryIdBySKU];
+            }
         }
 
         return $r->response;
